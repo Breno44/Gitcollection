b15ab8cdde9ea88e2fbe91291a192322da286cdb
Breno <bhfs.2908@gmail.com>
2021-12-03 14:55:37 -0300

    feat: routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Switch, Route } from "react-router-dom";
+import { Dashboard } from "../screens/Dashboard";
+import { Repo } from "../screens/Repo";
+
+export function Routes() {
+  return (
+    <Switch>
+      <Route path="/" component={Dashboard} exact />
+      <Route path="/repositories" component={Repo} />
+    </Switch>
+  );
+}
