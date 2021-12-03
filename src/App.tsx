5fcb6237ee87a83408e3c123c891c68e0133f3df
Breno <bhfs.2908@gmail.com>
2021-12-03 16:30:45 -0300

    feat: add Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./routes";
+import GlobalStyles from "./styles/global";
 
 export function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes />
     </BrowserRouter>
   );
